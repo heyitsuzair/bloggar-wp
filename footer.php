cd22ee326481c373f4526eead450441a9ddaf276
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-29 02:47:12 +0500

    My Testi Added

--- footer.php
+++ footer.php
@@ -9,6 +9,9 @@
 
 </div>
 </div>
+<?php
+get_template_part('template-parts/footer/pre_footer');
+?>
 <?php wp_footer(); ?>
 </body>
 
---
+++ template-parts/footer/pre_footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Pre Footer For the Theme.
+ *
+ * @package Bloggar_WP
+ */
+?>
+
+<main class="d-flex flex-column align-items-center gap-3 testimonial justify-content-center bg-light p-5">
+    <img src="<?php echo BLOGGAR_WP_DIR_URI . '/assets/img/me.jpg'; ?>"
+        class="<?php echo esc_attr('testimonial-img') ?>" alt="<?php echo esc_attr('Loading...') ?>"
+        loading="<?php echo esc_attr('lazy') ?>">
+    <h5>Muhammad Uzair</h5>
+    <a href="<?php echo esc_attr('//www.upwork.com/freelancers/~017b298c253a9c9bcb') ?>"
+        class="text-light fw-bold btn btn-info td-none" target="_blank">Hire Me <i class="fa fa-paper-plane"
+            aria-hidden="true"></i>
+    </a>
+</main>
